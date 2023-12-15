2a7a839696238c7697436605829cc676ab8ff99b
Numan Şenlik <nmnsnlk@gmail.com>
2023-12-16 02:43:42 +0300

    Navbar rigth basic build created

--- src/components/navbar/navbarItem/NavbarRight.tsx
+++ src/components/navbar/navbarItem/NavbarRight.tsx
@@ -1,12 +1,21 @@
-import {BiSearch} from 'react-icons/bi';
+import { BiSearch } from 'react-icons/bi';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { SlBasket } from 'react-icons/sl';
+
 
 function NavbarRight() {
-  return (
-    <div >
-        <div className="">
-            <input type="text" placeholder="Search.." />
+    return (
+        <div className="flex items-center gap-8">
+            <div className="flex items-center border p-3 rounded-full bg-gray-200 ">
+                <input type="text" className='bg-gray-200 outline-none' placeholder="Search.." />
+                <BiSearch size={28} />
+            </div>
+            <AiOutlineHeart />
+            <div className="relative">
+                <div className='absolute -top-3 rounded-full text-white bg-red-500 -right-3 w-5 h-5 flex items-center justify-center '>3</div>
+                <SlBasket size={28} />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 export default NavbarRight
