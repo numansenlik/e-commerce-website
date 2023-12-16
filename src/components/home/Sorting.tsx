e8b4c7a5a63a81d70d5a75ffd2d3753989c91424
Numan Şenlik <nmnsnlk@gmail.com>
2023-12-16 17:18:56 +0300

    Sorting comp created

---
+++ src/components/home/Sorting.tsx
@@ -0,0 +1,12 @@
+function Sorting() {
+    return (
+        <div className="bg-gray-100 my-5 p-5 flex items-center justify-end">
+            <select name="" id="">
+                <option disabled value="">Choose</option>
+                <option value="inc">İncrement</option>
+                <option value="dec">Decremeent</option>
+            </select>
+        </div>
+    )
+}
+export default Sorting
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import Category from "../components/home/Category"
+import Products from "../components/home/Products"
 import SliderComp from "../components/home/SliderComp"
+import Sorting from "../components/home/Sorting"
 
 function Home() {
     return (
         <div>
             <SliderComp />
+            <Sorting/>
+            <div className="flex">
+                <Category/>
+                <Products/>
+            </div>
         </div>
     )
 }
