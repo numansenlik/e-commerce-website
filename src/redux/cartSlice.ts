eb10411dd7c3134d6426d448975833356950802d
Numan Şenlik <nmnsnlk@gmail.com>
2023-12-20 05:09:33 +0300

    Created Cart Slice

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,21 +1,81 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const fetchFromLocalStroge = () => {
+interface ICartItem {
+    id: number;
+    title: string;
+    price: number;
+    quantity: number;
+    totalPrice: number;
+}
+
+interface ICartSlice {
+    carts: ICartItem[];
+    itemCount: number;
+    totalAmount: number;
+}
+
+const fetchFromLocalStroge = (): ICartItem[] => {
     let cart = localStorage.getItem('cart');
     if (cart) {
-        return JSON.parse(localStorage.getItem('cart'))
-    }else {
-    return []
+        return JSON.parse(cart);
+    } else {
+        return [];
     }
 }
 
-const initialState = {
-    cart: fetchFromLocalStroge(),
-    itemCount : 0,
+const storeInLocalStroge = (data: ICartItem[]): void => {
+    localStorage.setItem('cart', JSON.stringify(data));
+}
+
+const initialState: ICartSlice = {
+    carts: fetchFromLocalStroge(),
+    itemCount: 0,
     totalAmount: 0
 }
 
 const cartSlice = createSlice({
-name: "cart",
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<ICartItem>) => {
+            const isItemCart = state.carts.find(item => item.id === action.payload.id)
+            if (isItemCart) {
+                const tempCart = state.carts.map(item => {
+                    if (item.id === action.payload.id) {
+                        let tempQty = item.quantity + action.payload.quantity;
+                        let tempTotalPrice = tempQty * item.price;
+                        return {
+                            ...item, quantity: tempQty, totalPrice: tempTotalPrice
+                        }
+                    } else {
+                        return item
+                    }
+                })
 
+                state.carts = tempCart;
+                storeInLocalStroge(state.carts)
+            } else {
+                state.carts.push(action.payload)
+                storeInLocalStroge(state.carts)
+            }
+        },
+        removeFromCart: (state, action: PayloadAction<number>) => {
+            const tempCart = state.carts.filter(item => item.id !== action.payload)
+            state.carts = tempCart;
+            storeInLocalStroge(state.carts)
+        },
+        clearCart: (state) => {
+            state.carts = [];
+            storeInLocalStroge(state.carts)
+        },
+        getCartTotal: (state) => {
+            state.totalAmount = state.carts.reduce((cartTotal, cartItem) => {
+                return cartTotal += cartItem.totalPrice
+            }, 0)
+            state.itemCount = state.carts.length
+        }
+    }
 })
+
+export const { addToCart, removeFromCart, clearCart, getCartTotal } = cartSlice.actions
+export default cartSlice.reducer;
