5d2346fd9f5d8455d2d5f636c3f4dad02bcc2ab5
Numan Şenlik <nmnsnlk@gmail.com>
2023-12-22 04:09:16 +0300

    added clearCards button in Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { useEffect } from "react";
-import { getCartTotal } from "../redux/cartSlice";
+import { clearCart, getCartTotal } from "../redux/cartSlice";
 import CartsComp from "../components/cart/CartsComp";
 
 function Cart() {
   const dispatch = useDispatch();
-  const { carts, totalAmount, itemCount } = useSelector((state: RootState) => state.carts);
+  const { carts, totalAmount, itemCount  } = useSelector((state: RootState) => state.carts);
   console.log(carts, "/", totalAmount, "/", itemCount);
 
 
@@ -23,9 +23,13 @@ function Cart() {
               <CartsComp key={index} cart={cart} />
             ))
           }
+
+          <div className="flex justify-between">
+          <div onClick={()=> dispatch(clearCart())} className="bg-red-500 text-white w-[150px] h-12 flex items-center justify-center rounded-md cursor-pointer font-semibold" >Clear Cards</div>
           <div className="flex items-center justify-end text-xl">TOTAL PRICE: <span className="font-bold ml-2">{totalAmount} $</span></div>
+          </div>
         </div> :
-          <div>
+          <div className="text-center mt-32 text-3xl">
             Your cart is empty ...
           </div>
       }
